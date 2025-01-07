0105696b654c1499cf39c9e2b7f82a948e56e214
Wafa Bdeir <Wbdeir@oakland.edu>
2025-01-07 17:24:49 -0500

    Exclude my_project/ from tracking

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-=======
->>>>>>> 583ebe06887a923c9235058bcac580769c44b6d2
 
 ReactDOM.render(
   <React.StrictMode>
